8a864a77d42eb03ec9fd50d16d533f8a098e66cb
drewthoennes <dthoenne@purdue.edu>
2018-05-08 14:07:47 -0400

    Adds test cases

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
 const assert = require('assert');
 const replaceAt = require('./index.js');
+
+console.log('--- Testing ---');
+
+assert.deepEqual(replaceAt('I $ JS', 2, '<3'), 'I <3 JS');
+
+assert.deepEqual(replaceAt('$ is my name', 0, 'Coding'), 'Coding is my name');
+
+assert.deepEqual(replaceAt('Bet you can\'t finish this $', 26, 'sentence'), 'Bet you can\'t finish this sentence');
+
+console.log('All test cases passed');
